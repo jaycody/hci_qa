a4f21019fc96dab1963b04d57ed8ba423648e6d6
Jason Stephens <jason@catalogspree.com>
2014-08-21 20:21:25 -0700

    added boilerplate to site_load

--- py_test_cases/site_load.py
+++ py_test_cases/site_load.py
@@ -1,4 +1,16 @@
+#!/usr/bin/env python -tt
 # -*- coding: utf-8 -*-
+
+""" jstephens - tryouts: qa/support engineer - Healthy Communities Institute
+
+Script to verify successful loading of HCI website.
+
+"""
+
+
+
+
+
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
@@ -10,15 +22,24 @@ def is_alert_present(wd):
     except:
         return False
 
-class site_load(unittest.TestCase):
+class Test_site_load(unittest.TestCase):
+    """Added 'Test' to class name"""
+
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
     def test_site_load(self):
+        """Verify we've landed on the correct page"""
+
+        # set 'success' flag to True and wait to hear otherwise
         success = True
+        
         wd = self.wd
         wd.get("http://www.healthycommunitiesinstitute.com/")
+        
+        # Assert title matches expected
+        print "verifying webpage title"
         self.assertEqual("Healthy Communities Institute - The Leading Community Health Improvement Platform", wd.title)
         self.assertTrue(success)
     
