398c08c09e907f2789acd4ddb76c651b07c84203
Jason Stephens <jason@catalogspree.com>
2014-08-21 21:53:11 -0700

    added docstrings and boilerplat to the solution_hospital script

--- py_test_cases/solutions_hospital.py
+++ py_test_cases/solutions_hospital.py
@@ -1,4 +1,21 @@
+#!/usr/bin/env python -tt
 # -*- coding: utf-8 -*-
+
+""" jstephens - tryouts: qa/support engineer - Healthy Communities Institute
+
+Script to test the presence of the solutions-hospital webpage.
+
+ToDo:  
+[] Page Object Model for test cases
+[] Add Remote for off-site testing
+
+"""
+
+
+
+
+
+
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
@@ -10,23 +27,37 @@ def is_alert_present(wd):
     except:
         return False
 
-class solutions_hospital(unittest.TestCase):
+class Test_solutions_hospital(unittest.TestCase):
+    """Added 'Test' to the class name"""
+
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
     def test_solutions_hospital(self):
+        """Verify the presence of the solutions-hospital page"""
+
+        # set success flag to True and await otherwise
         success = True
+
         wd = self.wd
         wd.get("http://www.healthycommunitiesinstitute.com/")
+        
+        # Verify we've landed on the correct page
+        print "Verifying website title"
         self.assertEqual("Healthy Communities Institute - The Leading Community Health Improvement Platform", wd.title)
         ActionChains(wd).move_to_element(wd.find_element_by_link_text("SOLUTIONS")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_css_selector("ul.sub-menu")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_link_text("For Hospitals")).perform()
         wd.find_element_by_link_text("For Hospitals").click()
+        
+        # Verifying correct url
+        print "Verifying webpage url"
         if wd.current_url != "http://www.healthycommunitiesinstitute.com/hospital-solutions/":
             success = False
-            print("verifyCurrentUrl failed")
+            print "verifyCurrentUrl failed"
+        
+        # Did we make it all the way without triggering the flag?
         self.assertTrue(success)
     
     def tearDown(self):
