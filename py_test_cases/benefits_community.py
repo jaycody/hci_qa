684cad9753f841e7f5ec779249b79829770aabe2
Jason Stephens <jason@catalogspree.com>
2014-08-21 19:56:04 -0700

    added debug print statments at every assertion in webdriver test

--- py_test_cases/benefits_community.py
+++ py_test_cases/benefits_community.py
@@ -1,4 +1,17 @@
+#!/usr/bin/env python -tt
 # -*- coding: utf-8 -*-
+
+"""jstephens - tryouts: qa/support engineer - Healthy Communities Institute
+
+Script to verify the presence of the 'Join Our Community' drop down message 
+on the 'hospital-solutions' page
+
+
+run as unittest:
+$ python -m unittest -v benefits_community
+"""
+
+
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
@@ -16,21 +29,35 @@ class benefits_community(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_benefits_community(self):
+        
+        # Set 'success' flag as True -- to be tripped if any verification fails
         success = True
+        
         wd = self.wd
         wd.get("http://www.healthycommunitiesinstitute.com/")
+        
+        # Check that title matches expectation
         self.assertEqual("Healthy Communities Institute - The Leading Community Health Improvement Platform", wd.title)
+        print "Verifying page title matches expected", wd.title
+        
         ActionChains(wd).move_to_element(wd.find_element_by_link_text("SOLUTIONS")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_css_selector("ul.sub-menu")).perform()
         ActionChains(wd).move_to_element(wd.find_element_by_link_text("For Hospitals")).perform()
         wd.find_element_by_link_text("For Hospitals").click()
+        
+        # Check for expected URL
+        print "Verifying current url", wd.current_url
         if wd.current_url != "http://www.healthycommunitiesinstitute.com/hospital-solutions/":
             success = False
             print("verifyCurrentUrl failed")
         wd.find_element_by_xpath("//div[@class='post-content']/div[7]/div[1]/span").click()
+        
+        # Look for expected text in entire page  <!-- this could be more precise -->
+        print "Verifying the presence of specific text: 'is more than just technology'"
         if not ("is more than just technology" in wd.find_element_by_tag_name("html").text):
             success = False
             print("verifyTextPresent failed")
+        
         wd.find_element_by_xpath("//div[@class='post-content']/div[7]/div[1]/span").click()
         self.assertTrue(success)
     
